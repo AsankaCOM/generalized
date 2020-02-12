79abd676cb3f18b2c495dceaa8cfa4d64ff36b6d
ricardo <ricardo@nclouds.com>
2020-02-12 10:17:09 -0600

    adding changes to ECS Template

--- cloudformation/ECS/commands/create.sh
+++ cloudformation/ECS/commands/create.sh
@@ -2,4 +2,4 @@ cd "${BASH_SOURCE%/*}" || exit
 cd ..
 COMMAND=$(sed -e 's/:[^:\/\/]/="/g;s/$/"/g;s/ *=/=/g' config.yml) 
 eval $COMMAND 
-aws cloudformation create-stack --region us-west-2 --stack-name "$StackName" --template-body file://master.yml --parameters ParameterKey=KeyPair,ParameterValue="$KeyPair" --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --region $REGION --stack-name "$StackName" --template-body file://master.yml --parameters ParameterKey=KeyPair,ParameterValue="$KeyPair" ParameterKey=S3BucketName,ParameterValue="$TemplatesBucketName" ParameterKey=S3KeyPrefix,ParameterValue="$Prefix" --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
